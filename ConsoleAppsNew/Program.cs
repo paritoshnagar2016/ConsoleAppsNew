8c1746c2d746f70cc01bb7a7febffe5b47bef995
Paritosh Nagar <paritoshnagar2016@gmail.com>
2019-03-02 13:07:15 -0600

    Add project files.

---
+++ ConsoleAppsNew/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppsNew
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
